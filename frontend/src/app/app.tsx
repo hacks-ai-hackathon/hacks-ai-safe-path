80816510815a942d3c210b9d22cf88f4c5012d47
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-09-22 20:19:41 +0300

    frontend: init commit

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,11 @@
+import { Container, MantineProvider } from "@mantine/core";
+
+const App = () => {
+  return (
+      <MantineProvider>
+          <Container>App</Container>
+      </MantineProvider>
+  )
+}
+
+export default App;
