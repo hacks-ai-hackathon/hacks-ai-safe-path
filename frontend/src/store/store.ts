dcaaf6de199d94aeb000e225f268c5c95539e711
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-09-23 22:47:14 +0300

    frontend: add changes

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -6,16 +6,18 @@ type Store = {
     link: string;
     timeline: VideoTimeline;
     timelineStatus: "idle" | "loading" | "complete";
-    isAlert: boolean;
+    alertStatus: number;
     setLink: (link: string) => void;
+    setAlertStatus: (alert: number) => void;
     setVideoTimeline: (timeline: VideoTimeline) => void;
 }
 
 export const useAppStore = create<Store>((set) => ({
     link: '',
     timeline: [],
-    isAlert: true,
+    alertStatus: -1,
     timelineStatus: "complete",
     setLink: (link: string) => set({ link }),
+    setAlertStatus: (alertStatus: number) => set({ alertStatus }),
     setVideoTimeline: (timeline: VideoTimeline) => set({ timeline }),
 }));
