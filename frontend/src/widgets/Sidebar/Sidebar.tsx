42ac3e594aff0dd357a9cf94df0f4320f98a0f3b
danyaisyourhomie <danyakladnitsky@gmail.com>
2023-09-23 15:07:50 +0300

    frontend: adjust sidebar

--- frontend/src/widgets/Sidebar/Sidebar.tsx
+++ frontend/src/widgets/Sidebar/Sidebar.tsx
@@ -5,12 +5,13 @@ import styles from "./Sidebar.module.scss";
 const Sidebar = () => {
   return (
       <Card className={styles.sidebar}>
-        <Flex direction="column" gap="lg" className={styles.layout}>
+          <Flex direction="column" className={styles.layout}>
               <Group>
+                <Text fw="bold" size="lg">Мониторинг</Text>
                 <Alert icon={<IconAlertTriangleFilled />} color="yellow" w="100%" fw="bold" variant="filled">
                     Внимание!
                 </Alert>
-                  <Timeline active={0} bulletSize={24} lineWidth={3}>
+                  <Timeline active={1} bulletSize={24} lineWidth={3}>
                 <Timeline.Item color="yellow" bullet={<IconEye size={18} />} fz="sm" fw="bold" title="Замечен человек">
                     <Flex direction="column">
                         <Text c="dimmed" size="xs">Правая сторона движения</Text>
@@ -18,8 +19,8 @@ const Sidebar = () => {
                         <Image mt="sm" radius="sm" src="https://static.mk.ru/upload/entities/2020/03/04/19/articles/detailPicture/66/ff/09/3e/04d7f13e955d2ad85439a3ea2eb29977.jpg" />
                     </Flex>
                 </Timeline.Item>
-                <Timeline.Item bullet={<IconBellSchool size={14} />} fz="xs" title="Предупредительный сигнал">
-                    <Text c="dimmed" size="xs">Через 8 секунд подайте сигнал человеку</Text>
+                <Timeline.Item color="yellow" bullet={<IconBellSchool size={14} />} fz="xs" title="Предупредительный сигнал">
+                    <Text c="dimmed" size="xs">Подайте сигнал человеку</Text>
                 </Timeline.Item>
                 <Timeline.Item  opacity="0.6" bullet={<IconSquareRoundedX size={14} />} fz="xs" title="Полная остановка">
                         <Text c="dimmed" size="xs">Через 20 секунд остановите поезд</Text>
@@ -29,7 +30,7 @@ const Sidebar = () => {
                 </Timeline.Item>
               </Timeline>
             </Group>
-            <Button color="green" className={styles.ringBtn} leftSection={<IconBellSchool size={24} />}>Подать сигнал</Button>
+            <Button color="yellow" size="md" className={styles.ringBtn} leftSection={<IconBellSchool size={24} />}>Подать сигнал</Button>
         </Flex>
     </Card>
   )
